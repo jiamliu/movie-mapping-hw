978a47a33f0be574d4d5ffbf171ced59c68f8cb1
Jeremy Taubman <56704648+taubman33@users.noreply.github.com>
2023-02-15 16:16:42 -0500

    Update Movie.jsx

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,28 +1,13 @@
-import { useState } from 'react'
+
 import Genre from "./Genre"
 import Overview from "./Overview"
 
-const Movie = ({ movie }) => {
-  const [showOverview, setShowOverview] = useState(false)
 
-  const toggleOverview = () => {
-    if (showOverview) {
-      setShowOverview(false)
-    } else {
-      setShowOverview(true)
-    }
-  }
 
   return (
     <div className="poster">
-      {showOverview ? <Overview toggleOverview={toggleOverview} overview={movie.overview} backdrop={movie.backdrop_path} /> : <img src={movie.poster_path} alt="poster" onClick={toggleOverview} />}
-      <h2 className="movie-title">{movie.title}</h2>
-      <h5 className="movie-date">Release Date: {movie.release_date}</h5>
-      <div className="genres">
-        {movie.genres.map((genre) => (
-          <Genre key={genre.id} genre={genre} />
-        ))}
-      </div>
+     
+
     </div>
   )
 }
